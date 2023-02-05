4524c5b9dad905cef9dfc36ddc0e4016dc715056
Evgeniy Dolgov <amyr214519@gmail.com>
2023-02-05 15:01:32 +0300

    feat: add year and rs school link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,13 @@ export const Footer = () => (
   <footer className="footer">
     <div className="footer-wrapper">
       <ul className="github-list">
+        <li className="github-list__name">Authors:</li>
         <li><a href="https://github.com/evgeniydolgov"><div className="git-img">{}</div></a></li>
         <li><a href="https://github.com/harry177"><div className="git-img">{}</div></a></li>
         <li><a href="https://github.com/grishanova-oa"><div className="git-img">{}</div></a></li>
       </ul>
-      <div className="test-div" />
+      <div className="year-creation">2023 ©</div>
+      <div className="rs-container"><a href="https://github.com/grishanova-oa"><div className="rs-link">{}</div></a></div>
     </div>
   </footer>
 );
