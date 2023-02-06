fd201a76c8acde60883fd936bfed4bb3765ebbd7
grishanova-oa <grishanova.oa@gmail.com>
2023-02-06 21:23:08 +0100

    feat: add library ReactVideo

--- src/components/RecipeVideo/RecipeVideo.tsx
+++ src/components/RecipeVideo/RecipeVideo.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './RecipeVideoStyles.css';
+import ReactPlayer from 'react-player';
 
 export const RecipeVideo = () => (
   <div className="recipe-video">
-    <h3>Video recipe </h3>
+    <h3>
+      Video recipe:
+      <span> Spicy Arrabiata Penne</span>
+    </h3>
+    <ReactPlayer
+      url="https://www.youtube.com/watch?v=1IszT_guI08"
+      width="600px"
+      height="400px"
+      controls
+    />
   </div>
 );
