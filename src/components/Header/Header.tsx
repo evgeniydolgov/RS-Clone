0c747c6946ad0411fd840fea0a7dacba66c976dd
grishanova-oa <grishanova.oa@gmail.com>
2023-02-07 15:11:56 +0100

    fix: fix size header and footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,20 @@ import './HeaderStyles.css';
 
 export const Header = () => (
   <header>
-    <div className="header-info">
-      <div className="header-logo" />
-      <nav>
-        <ul className="header-list">
-          <li><a className="header-link" href="/#">Cooking Quiz</a></li>
-          <li><a className="header-link" href="/#">Recipes</a></li>
-        </ul>
-      </nav>
-    </div>
-    <div className="header-authorization">
-      <button type="button" className="btn authorization__btn">Login</button>
-      <button type="button" className="btn authorization__btn">Sign Up</button>
+    <div className="header__inner">
+      <div className="header-info">
+        <div className="header-logo" />
+        <nav>
+          <ul className="header-list">
+            <li><a className="header-link" href="/#">Cooking Quiz</a></li>
+            <li><a className="header-link" href="/#">Recipes</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="header-authorization">
+        <button type="button" className="btn authorization__btn">Login</button>
+        <button type="button" className="btn authorization__btn">Sign Up</button>
+      </div>
     </div>
   </header>
 );
