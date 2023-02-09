fed440db2c3a8a5cabf83a88a4f7116fabeda935
grishanova-oa <grishanova.oa@gmail.com>
2023-02-09 15:30:32 +0100

    fix: fix rules in commonActions

--- src/store/actions/commonActions.ts
+++ src/store/actions/commonActions.ts
@@ -1,27 +1,23 @@
 import { useDispatch } from 'react-redux';
 import { api } from '../../Api';
 import { AppDispatch } from '../../types';
-import { saveAllCuisine, saveRandomSelection, saveRecipeById } from '../slises/commonSlice';
+import { saveAllCuisine, saveRandomSelection, saveActiveRecipe } from '../slises/commonSlice';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-const IDispatch = typeof useAppDispatch;
 
-export const loadAllCuisine = () => async (dispatch: typeof useAppDispatch): Promise<void> => {
+export const loadAllCuisine = () => async (dispatch: AppDispatch): Promise<void> => {
   const date = await api.loadAllCuisine();
 
-  // @ts-ignore
   dispatch(saveAllCuisine(date.meals));
 };
 
-export const loadRandomSelection = () => async (dispatch: typeof useAppDispatch): Promise<void> => {
+export const loadRandomSelection = () => async (dispatch: AppDispatch): Promise<void> => {
   const date = await api.loadRandomSelection();
 
-  // @ts-ignore
   dispatch(saveRandomSelection(date.meals));
 };
-export const loadRecipeById = (id: number) => async (dispatch: typeof IDispatch): Promise<void> => {
+export const loadRecipeById = (id: number) => async (dispatch: AppDispatch): Promise<void> => {
   const date = await api.loadRecipeById(id);
 
-  // @ts-ignore
-  dispatch(saveRecipeById(date.meals));
+  dispatch(saveActiveRecipe(date.meals[0]));
 };
