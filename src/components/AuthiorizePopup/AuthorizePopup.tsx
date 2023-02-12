1338922a94b03ae1901aebc9f6754bce6e0807b5
Harry177 <harry177@rambler.ru>
2023-02-12 08:02:09 +0100

    feat: add registration through authorization popup; fix: change mysql settings

--- src/components/AuthiorizePopup/AuthorizePopup.tsx
+++ src/components/AuthiorizePopup/AuthorizePopup.tsx
@@ -3,7 +3,14 @@ import './AuthorizePopup.css';
 
 export const AuthorizePopup = ({ active, setActive, children }: Props) => (
   <div className={active ? 'authorize__popup active' : 'authorize__popup'} onMouseDown={() => setActive(false)} role="button" tabIndex={0} aria-label="signup">
-    <div className={active ? 'popup__inner active' : 'popup__inner'} onMouseDown={(item) => item.stopPropagation()} role="button" tabIndex={0} aria-label="signup">
+    <div
+      className={active
+        ? 'popup__inner active' : 'popup__inner'}
+      onMouseDown={(item) => item.stopPropagation()}
+      role="button"
+      tabIndex={0}
+      aria-label="signup"
+    >
       {children}
     </div>
   </div>
