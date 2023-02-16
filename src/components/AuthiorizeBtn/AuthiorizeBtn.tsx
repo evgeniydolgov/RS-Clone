8a7ad93d92aeda1f4e71dfbc604f4e3e7b7a5890
Evgeniy Dolgov <amyr214519@gmail.com>
2023-02-16 11:45:46 +0300

    fix: restyle login buttons

---
+++ src/components/AuthiorizeBtn/AuthiorizeBtn.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface IloginBtn {
+  textBtn: string;
+  className: string
+  openPopUp: (el: boolean) => void;
+  setUserLogged: (el: string) => void;
+}
+
+export const AuthiorizeBtn = ({
+  textBtn, className, openPopUp, setUserLogged,
+}: IloginBtn) => {
+  const logOutUser: React.MouseEventHandler<HTMLButtonElement> = (element) => {
+    if (element.currentTarget.textContent === 'LogIn') {
+      openPopUp(true);
+      return;
+    }
+    localStorage.clear();
+    setUserLogged('LogIn');
+  };
+
+  return (
+    <button type="button" className={className} onClick={logOutUser}>
+      <Link className="btn__link" to="/authorize">{textBtn}</Link>
+      <div className="icon__button" />
+    </button>
+  );
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HeaderStyles.css';
@@ -12,7 +13,6 @@ export const Header = () => {
       if (localStorage.getItem('user') != null) {
         // console.log('dfgdg');
         setUser(localStorage.getItem('user'));
-        /* change logout */
       }
     };
     window.addEventListener('storage', storageEventHandler);
@@ -56,8 +56,9 @@ export const Header = () => {
           </div>
         )}
         <div className="header-authorization">
-          <button type="button" className="btn authorization__btn"><Link className="btn__link" to="/authorize">Login</Link></button>
-          <button type="button" className="btn authorization__btn"><Link className="btn__link" to="/authorize">Sign Up</Link></button>
+          <button type="button" className="btn authorization__btn">
+            <Link className="btn__link" to="/authorize">Join us</Link>
+          </button>
         </div>
       </div>
     </header>
