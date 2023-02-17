61e51cf434aa23c885b9495a0d4fcdaf2a623390
Evgeniy Dolgov <amyr214519@gmail.com>
2023-02-17 10:55:50 +0300

    feat: add buy/refusal sounds

--- src/components/ShopCard/ShopCard.tsx
+++ src/components/ShopCard/ShopCard.tsx
@@ -8,34 +8,43 @@ interface ICard {
 }
 
 export const ShopCard = ({ nameSloth, index }: ICard) => {
-  const Avatar = 'Avatar: ';
+  // const Avatar = 'Avatar: ';
   const costs = (index + 1) * 10;
   const priceMessage = `Price: ${costs} scores`;
 
-  const winMusic = () => {
+  const noMoneyMusic = () => {
     const path = require('../../assets/mp3/necromancernogold1.mp3');
+    return new Audio(path);
+  };
+
+  const purchaseApproved = () => {
+    const path = require('../../assets/mp3/cash-sound.mp3');
     return new Audio(path);
   };
 
   const buyAvatar: React.MouseEventHandler<HTMLElement> = (el) => {
     if (Number(el.currentTarget.dataset.price) < 35) {
       console.log('sell!');
+      purchaseApproved().play();
+      el.currentTarget.className = 'card__container purchase';
+      el.currentTarget.children[0].className = 'black-line goodmessage';
     } else {
       console.log('not enough money');
       el.currentTarget.className = 'card__container shaker';
-      winMusic().play();
-      console.log();
+      noMoneyMusic().play();
+      el.currentTarget.children[0].className = 'black-line wrongmessage';
     }
   };
 
   const defautClass: React.MouseEventHandler<HTMLElement> = (el) => {
     el.currentTarget.className = 'card__container';
+    el.currentTarget.children[0].className = 'black-line';
   };
 
   return (
     <div role="presentation" data-price={costs} className="card__container" onClick={buyAvatar} onMouseLeave={defautClass}>
       <div className="black-line">
-        <p>{Avatar + nameSloth}</p>
+        {/* <p>{Avatar + nameSloth}</p> */}
         <p>{priceMessage}</p>
       </div>
       <div className={nameSloth} />
