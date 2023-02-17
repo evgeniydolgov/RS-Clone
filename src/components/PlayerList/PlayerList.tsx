8ec5dbc337b54ca52fc516a75888dad41338110a
Evgeniy Dolgov <amyr214519@gmail.com>
2023-02-17 16:34:30 +0300

    feat: render player list

--- src/components/PlayerList/PlayerList.tsx
+++ src/components/PlayerList/PlayerList.tsx
@@ -1,5 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import { PlayerInfo } from '../PlayerInfo';
+import './PlayerListStyle.css';
 
-export const PlayerList = () => (
-  <div>PlayerList</div>
-);
+export const PlayerList = () => {
+  const testArr = [1, 2, 1];
+  return (
+    <div className="play-list">
+      <div>Our The Best culinary experts</div>
+      <ol>
+        <PlayerInfo />
+      </ol>
+    </div>
+  );
+};
