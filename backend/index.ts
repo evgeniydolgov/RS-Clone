83a35cb11864fbfff962d8ffb35870a86b6bc80c
Harry177 <harry177@rambler.ru>
2023-02-26 19:20:18 +0100

    refactor: update cors config

--- backend/index.ts
+++ backend/index.ts
@@ -23,6 +23,14 @@ app.use(bodyParser.urlencoded({ extended: true } as OptionsUrlencoded | undefine
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 const urlencodedParser = express.urlencoded({ extended: false } as OptionsUrlencoded | undefined);
 
 app.listen(PORT as number, () => {
